c1bb0c966015369f641f01ee0c9d06197ec5397f
sanjeev <3292092+sanjeev40084@users.noreply.github.com>
2026-01-07 14:49:21 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'discount-calc-v14';
+const CACHE_NAME = 'discount-calc-v15';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -28,6 +28,7 @@ self.addEventListener('fetch', (e) => {
     );
 
 });
+
 
 
 
